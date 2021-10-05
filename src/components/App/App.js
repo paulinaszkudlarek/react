e2b3399de11bb728a28f2b5f4f0d9f0be02f8180
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2021-10-05 17:42:35 +0200

    Change class Hero in Hero.js to function Hero

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,7 +8,9 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>My first React app</h1>
         <h1 className={styles.subtitle}>Hello world!</h1>
-        <List />
+        <List title={['My list to do', <sup key='1'>soon!</sup>]} src={"http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"}>
+          <p>I'm planning on doing all these things sooner, rather than later!</p>  
+        </List>
       </main>
     )
   }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node,
+    src: PropTypes.string,
+  }
+
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  }
+
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} src={this.props.src} />
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
       </section>
     )
   }
