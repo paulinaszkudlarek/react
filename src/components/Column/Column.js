739e10b84033c4714a3bd5878fb53cb89e2adee3
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2022-03-14 23:01:20 +0100

    Refactor class component Column.js to functional component

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,42 +1,35 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-//import { settings } from '../../data/dataStore.js';
+import { settings } from '../../data/dataStore.js';
 import Creator from '../Creator/Creator.js';
 import Card from '../Card/Card.js';
-import Icon from '../Icon/Icon';
-import { settings } from '../../data/dataStore';
+import Icon from '../Icon/Icon.js';
 
 
-class Column extends React.Component {
+const Column = props => (
+  <section className={styles.component}>
+    <h3 className={styles.title}><span className={styles.icon}><Icon name={props.icon} /></span>{props.title}</h3>
+    <div className={styles.cards}>
+      {props.cards.map( cardData => (
+        <Card key={cardData.id} {...cardData} />
+      ))}        
+    </div>
+    <div className={styles.Creator}>
+      <Creator action={props.addCard} text={settings.cardCreatorText}  />
+    </div>
+  </section>
+);
 
-  static propTypes = {
-    cards: PropTypes.array,
-    icon: PropTypes.string,
-    title: PropTypes.string,
-    addCard: PropTypes.func,
-  }
+Column.propTypes = {
+  cards: PropTypes.array,
+  icon: PropTypes.string,
+  title: PropTypes.string,
+  addCard: PropTypes.func,
+};
 
-  static defaultProps = {
-    icon: settings.defaultColumnIcon,
-  }
-
-  render () {
-    const {icon, title, cards, addCard} = this.props;
-    return (
-      <section className={styles.component}>
-        <h3 className={styles.title}><span className={styles.icon}><Icon name={icon} /></span>{title}</h3>
-        <div className={styles.cards}>
-          {cards.map( cardData => (
-            <Card key={cardData.id} {...cardData} />
-          ))}        
-        </div>
-        <div className={styles.Creator}>
-          <Creator action={addCard} text={settings.cardCreatorText}  />
-        </div>
-      </section>
-    );
-  }
-}
+Column.defaultProps = {
+  icon: settings.defaultColumnIcon,
+};
 
 export default Column;
