3728e74ca5af9ae5ed3ae48717ef56a29c753ffd
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2022-03-16 18:19:45 +0100

    Install react router, add publicPath and devServerto webpack.config.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,23 +6,25 @@ import PropTypes from 'prop-types';
 import {settings} from '../../data/dataStore.js';
 import ReactHtmlParser from 'react-html-parser';
 import Creator from '../Creator/Creator.js';
+import Container from '../Container/Container.js';
 
 const List = props => (
-//    const {title, image, description, columns, addColumn} = this.props;
-  <section className={styles.component}>
-    <Hero titleText={props.title} src={props.image} />
-    <div className={styles.description}>
-      {ReactHtmlParser(props.description)}
-    </div>
-    <div className={styles.columns}>
-      {props.columns.map(columnData => (
-        <Column key={columnData.id} {...columnData} />
-      ))}        
-    </div>
-    <div className={styles.Creator}>
-      <Creator text={settings.columnCreatorText} action={props.addColumn} />
-    </div>
-  </section>
+  <Container>
+    <section className={styles.component}>
+      <Hero titleText={props.title} src={props.image} />
+      <div className={styles.description}>
+        {ReactHtmlParser(props.description)}
+      </div>
+      <div className={styles.columns}>
+        {props.columns.map(columnData => (
+          <Column key={columnData.id} {...columnData} />
+        ))}        
+      </div>
+      <div className={styles.Creator}>
+        <Creator text={settings.columnCreatorText} action={props.addColumn} />
+      </div>
+    </section>
+  </Container>
 );
 
 List.propTypes = {
