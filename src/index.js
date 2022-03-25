c56b33b5d4a0237fb5707e2896761c28b8efdae1
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2022-03-25 22:49:39 +0100

    Install bootstrap, import bootsrap in index.js

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App.js';
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
