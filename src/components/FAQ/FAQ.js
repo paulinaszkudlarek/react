b3ed815d3267eae2ced1690a80100dea2c0c348a
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2022-03-30 18:56:54 +0200

    Add accordion to faq.js from react-bootstrap, delete all styles from FAQ.scss

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -3,19 +3,41 @@ import { settings } from '../../data/dataStore';
 import Container from '../Container/Container';
 import Hero from '../Hero/Hero';
 import PropTypes from 'prop-types';
-import Faq from 'react-faq-component';
-import styles from './FAQ.scss';
+import { Accordion } from 'react-bootstrap';
 
 const FAQ = (props) => (
   <Container>
     <Hero titleText={settings.faq.title} src={props.src} ></Hero>
-    <Faq className={styles} config={config} data={settings.faq.list}></Faq>
+    <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Collapse >
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+            est laborum.
+          </Accordion.Body>
+        </Accordion.Collapse>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
   </Container>
 );
-
-const config = {
-  tabFocus: true,
-};
 
 FAQ.propTypes = {
   titleText: PropTypes.string,
