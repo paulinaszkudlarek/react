ed795c3b3eb410ac1c584e9f20c32a8c7a328c95
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2022-04-07 16:21:03 +0200

    Delete component accordion.collapse, fix importing of image source in component hero

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -5,23 +5,21 @@ import Hero from '../Hero/Hero';
 import PropTypes from 'prop-types';
 import { Accordion } from 'react-bootstrap';
 
-const FAQ = (props) => (
+const FAQ = () => (
   <Container>
-    <Hero titleText={settings.faq.title} src={props.src} ></Hero>
+    <Hero titleText={settings.faq.title} src={settings.defaultHeroImage} ></Hero>
     <Accordion defaultActiveKey="0">
       <Accordion.Item eventKey="0">
         <Accordion.Header>Accordion Item #1</Accordion.Header>
-        <Accordion.Collapse >
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-            est laborum.
-          </Accordion.Body>
-        </Accordion.Collapse>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
         <Accordion.Header>Accordion Item #2</Accordion.Header>
