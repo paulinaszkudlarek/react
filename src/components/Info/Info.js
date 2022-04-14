b8b8c312712d33ce30c09fd1e2003dcde2eb51c5
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2022-04-15 00:29:42 +0200

    Change import of info in Info.js

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { settings } from '../../data/dataStore.js';
+import { settings, info } from '../../data/dataStore.js';
 import Container from '../Container/Container.js';
 import Hero from '../Hero/Hero.js';
 
 const Info = () => (
   <Container>
-    <Hero titleText={settings.info.title} src={settings.defaultHeroImage}></Hero>
-    <p>{settings.info.content}</p>
+    <Hero titleText={info.title} src={settings.defaultHeroImage}></Hero>
+    <p>{info.content}</p>
   </Container>
 );
 
