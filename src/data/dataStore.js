9be9695eff91ddc51cba92e351fbc68102884cd4
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2022-04-23 13:58:23 +0200

    Add list-2 and list-3 to dadaStore.js

--- src/data/dataStore.js
+++ src/data/dataStore.js
@@ -117,6 +117,19 @@ const lists = [
     description: 'Interesting things I want to check out!',
     image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
   },
+  {
+    id: 'list-2',
+    title: 'Things to do <sup>tomorrow.</sup>',
+    description: 'Less interesting things I want to check out.',
+    image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
+  },
+  {
+    id: 'list-3',
+    title: 'Things to do <sup>someday...</sup>',
+    description: 'Non interesting things I want to check out...',
+    image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
+  },
+
 ];
 
 const columns = [
